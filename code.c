b008ed3775d358bfb6d028cc27f3e51ddce18566
PsGhosh <pgsarothi2005@gmail.com>
2025-09-07 14:58:49 +0600

    first commit

---
+++ code.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+// all assignmenrt operators
+//  =, +=, -=, *=, /=, %=, <<=, >>=, &=, ^=, |=
+int main()
+{
+    int a = 10;
+    int b = 20;
+    int c = 0;
+
+    c = a + b; // simple assignment operator
+    printf("Value of c = %d\n", c);
+
+    c += a; // c = c + a
+    printf("Value of c after += is %d\n", c);
+
+    c -= a; // c = c - a
+    printf("Value of c after -= is %d\n", c);
+
+    c *= a; // c = c * a
+    printf("Value of c after *= is %d\n", c);
+
+    c /= a; // c = c / a
+    printf("Value of c after /= is %d\n", c);
+
+    c %= a; // c = c % a
+    printf("Value of c after %%= is %d\n", c);
+
+    c <<= 2; // c = c << 2
+    printf("Value of c after <<= is %d\n", c);
+
+    c >>= 2; // c = c >> 2
+    printf("Value of c after >>= is %d\n", c);
+
+    c &= 2; // c = c & 2
+    printf("Value of c after &= is %d\n", c);
+
+    c ^= 2; // c = c ^ 2
+    printf("Value of c after ^= is %d\n", c);
+
+    c |= 2; // c = c | 2
+    printf("Value of c after |= is %d\n", c);
+
+    return 0;
+}
