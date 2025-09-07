bc8ae45d2b998088403c667401894423729778fc
PsGhosh <pgsarothi2005@gmail.com>
2025-09-07 15:07:14 +0600

    HomeWork question

--- code.c
+++ code.c
@@ -1,47 +1,55 @@
+// #include <stdio.h>
+
+// // all assignmenrt operators
+// //  =, +=, -=, *=, /=, %=, <<=, >>=, &=, ^=, |=
+// int main()
+// {
+//     int a = 10;
+//     int b = 20;
+//     int c = 0;
+
+//     c = a + b; // simple assignment operator
+//     printf("Value of c = %d\n", c);
+
+//     c += a; // c = c + a
+//     printf("Value of c after += is %d\n", c);
+
+//     c -= a; // c = c - a
+//     printf("Value of c after -= is %d\n", c);
+
+//     c *= a; // c = c * a
+//     printf("Value of c after *= is %d\n", c);
+
+//     c /= a; // c = c / a
+//     printf("Value of c after /= is %d\n", c);
+
+//     c %= a; // c = c % a
+//     printf("Value of c after %%= is %d\n", c);
+
+//     c <<= 2; // c = c << 2
+//     printf("Value of c after <<= is %d\n", c);
+
+//     c >>= 2; // c = c >> 2
+//     printf("Value of c after >>= is %d\n", c);
+
+//     c &= 2; // c = c & 2
+//     printf("Value of c after &= is %d\n", c);
+
+//     c ^= 2; // c = c ^ 2
+//     printf("Value of c after ^= is %d\n", c);
+
+//     c |= 2; // c = c | 2
+//     printf("Value of c after |= is %d\n", c);
+
+//     return 0;
+//     // Output:
+//     // Value of c = 30
+// }
+            
 #include <stdio.h>
-
-// all assignmenrt operators
-//  =, +=, -=, *=, /=, %=, <<=, >>=, &=, ^=, |=
-int main()
-{
-    int a = 10;
-    int b = 20;
-    int c = 0;
-
-    c = a + b; // simple assignment operator
-    printf("Value of c = %d\n", c);
-
-    c += a; // c = c + a
-    printf("Value of c after += is %d\n", c);
-
-    c -= a; // c = c - a
-    printf("Value of c after -= is %d\n", c);
-
-    c *= a; // c = c * a
-    printf("Value of c after *= is %d\n", c);
-
-    c /= a; // c = c / a
-    printf("Value of c after /= is %d\n", c);
-
-    c %= a; // c = c % a
-    printf("Value of c after %%= is %d\n", c);
-
-    c <<= 2; // c = c << 2
-    printf("Value of c after <<= is %d\n", c);
-
-    c >>= 2; // c = c >> 2
-    printf("Value of c after >>= is %d\n", c);
-
-    c &= 2; // c = c & 2
-    printf("Value of c after &= is %d\n", c);
-
-    c ^= 2; // c = c ^ 2
-    printf("Value of c after ^= is %d\n", c);
-
-    c |= 2; // c = c | 2
-    printf("Value of c after |= is %d\n", c);
-
-    return 0;
-    // Output:
-    // Value of c = 30
+int main(){
+    char a=7;
+    a^=5;
+    printf("%d",printf ("%d",a+=3));
+        return 0;
 }
